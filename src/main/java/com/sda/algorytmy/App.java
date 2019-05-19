be75c9742a45978a042a20ebab2b488d6fc4eb70
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-19 11:16:35 +0100

    zmiany

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -22,33 +22,34 @@ public class App {
             }
         }
 
-            System.out.println(Arrays.toString(tab));
-            return tab;
-
-        }
-        public static void main (String[]args) throws InterruptedException {
-            Random random = new Random();
-            int min = -5000;
-            int max = 5000;
-            int[] tab = new int[20];
-            long startTime = System.currentTimeMillis();
-            for (int i = 0; i < tab.length; i++) {
-                int randomNumber = random.nextInt(max - min + 1) + min;
-                tab[i] = randomNumber;
-            }
-
-            long endTime = System.currentTimeMillis();
-            long totalTime = endTime - startTime;
-            System.out.println("Czas wykonywania" + " " + totalTime);
-            startTime = System.currentTimeMillis();
-            sort(tab);
-            endTime = System.currentTimeMillis();
-            totalTime = endTime - startTime;
-            System.out.println("Czas sortowania:" + totalTime);
-
-        }
+        System.out.println(Arrays.toString(tab));
+        return tab;
 
     }
+
+    public static void main(String[] args) throws InterruptedException {
+        Random random = new Random();
+        int min = -5000;
+        int max = 5000;
+        int[] tab = new int[20];
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < tab.length; i++) {
+            int randomNumber = random.nextInt(max - min + 1) + min;
+            tab[i] = randomNumber;
+        }
+
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("Czas wykonywania" + " " + totalTime);
+        startTime = System.currentTimeMillis();
+        sort(tab);
+        endTime = System.currentTimeMillis();
+        totalTime = endTime - startTime;
+        System.out.println("Czas sortowania:" + totalTime);
+
+    }
+
+}
 
 
 
