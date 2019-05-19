b5433d83451849e5b6e73913e04aca000c5c204b
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-19 12:34:58 +0100

    kolejne zmiany

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -13,58 +13,60 @@ public class App {
 
         int counter = 0;
         boolean flaga = false;
+        for (int j = 0; j < tab.length - j - 1; j++) {
 
-        for (int j = 0; j < tab.length; j++) {
-            for (int i = 0; i < tab.length - j - 1; i++) {
-                if (tab[i] > tab[i + 1]) {
-                    int tmp = tab[i + 1];
-                    tab[i + 1] = tab[i];
-                    tab[i] = tmp;
-                    flaga = true;
+            for (int i = 0; i < tab.length - j -1; i++) {
+
+                    if (tab[i] > tab[i + 1]) {
+                        int tmp = tab[i + 1];
+                        tab[i + 1] = tab[i];
+                        tab[i] = tmp;
+                        flaga = true;
+                    }
+                    counter++;
                 }
-                counter++;
+
+                if (!flaga) {
+                    break;
+                }
             }
 
-            if (!flaga) {
-                break;
+            System.out.println(Arrays.toString(tab));
+            System.out.println(counter);
+            return tab;
+
+        }
+
+
+
+        public static void main (String[]args) throws InterruptedException {
+            Random random = new Random();
+            int min = -5000;
+            int max = 5000;
+            int[] tab = new int[20];
+            long startTime = System.currentTimeMillis();
+            for (int i = 0; i < tab.length; i++) {
+                int randomNumber = random.nextInt(max - min + 1) + min;
+                tab[i] = randomNumber;
             }
+
+            long endTime = System.currentTimeMillis();
+            long totalTime = endTime - startTime;
+            System.out.println("Czas wykonywania" + " " + totalTime);
+            startTime = System.currentTimeMillis();
+            sort(tab);
+            endTime = System.currentTimeMillis();
+            totalTime = endTime - startTime;
+            System.out.println("Czas sortowania:" + totalTime);
+
         }
 
-        System.out.println(Arrays.toString(tab));
-        System.out.println(counter);
-        return tab;
-
     }
-
-    public static void main(String[] args) throws InterruptedException {
-        Random random = new Random();
-        int min = -5000;
-        int max = 5000;
-        int[] tab = new int[20];
-        long startTime = System.currentTimeMillis();
-        for (int i = 0; i < tab.length; i++) {
-            int randomNumber = random.nextInt(max - min + 1) + min;
-            tab[i] = randomNumber;
-        }
-
-        long endTime = System.currentTimeMillis();
-        long totalTime = endTime - startTime;
-        System.out.println("Czas wykonywania" + " " + totalTime);
-        startTime = System.currentTimeMillis();
-        sort(tab);
-        endTime = System.currentTimeMillis();
-        totalTime = endTime - startTime;
-        System.out.println("Czas sortowania:" + totalTime);
-
-    }
-
-}
 
 
 
 
 /*}while (true) {
-
             System.out.println(randomNumber);
             if (randomNumber == min) {
                 break;
